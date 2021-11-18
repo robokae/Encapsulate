b4d13b364fb4f0396333b16be6a4d3f91a85cbc4
robokae <alexanderhom19@gmail.com>
2021-11-17 17:49:34 -0800

    Updated database values

--- api/db/db.sql
+++ api/db/db.sql
@@ -1,4 +1,14 @@
--- drop
+-- =======================================================
+-- CSE 111 Project Phase 2
+-- Author: Alexander Hom, Joshua Stickles
+-- Description: 
+    -- Create and populate tables. 
+    -- Query, update, and delete values from tables.   
+-- =======================================================
+
+-- =================
+-- DROP TABLES
+-- =================
 drop table Users;
 drop table Posts;
 drop table Liked;
@@ -8,7 +18,9 @@ drop table Topics;
 drop table PostsWithTopics;
 drop table Followers;
 
--- create
+-- ==================
+-- CREATE TABLES
+-- ==================
 create table Users (
     user_id int primary key,
     user_name varchar(100) not null,
@@ -58,7 +70,9 @@ create table Followers(
 	follower_following_id int not null 
 );
 
--- insert
+-- ================================
+-- INSERT VALUES INTO TABLES
+-- ================================
 insert into Users values (1, 'Cecylia Dumas', 'Hi, my name is Cecylia Dumas', 0);
 insert into Users values (2, 'Jeannette McDermott', 'Hi, my name is Jeannette McDermott', 1);
 insert into Users values (3, 'Stipo Fabian', 'Hi, my name is Stipo Fabian', 1);
@@ -70,16 +84,16 @@ insert into Users values (8, 'Lennie Mac Dhuibhinse', 'Hi, my name is Lennie Mac
 insert into Users values (9, 'Tomomi Stanković', 'Hi, my name is Tomomi Stanković', 1);
 insert into Users values (10, 'Tyrrell Coelho', 'Hi, my name is Tyrrell Coelho', 1);
 
-insert into Posts values (1, 6, '11-1-2021', 3, 'Post text from user 6');
-insert into Posts values (2, 10, '11-3-2021', 1, 'Post text from user 10');
-insert into Posts values (3, 9,	'11-4-2021', 5,	'Post text from user 9');
-insert into Posts values (4, 7, '11-6-2021', 9, 'Post text from user 7');
-insert into Posts values (5, 6, '11-7-2021', 10, 'Post text from user 6');
-insert into Posts values (6, 3, '11-8-2021', 2, 'Post text from user 3');
-insert into Posts values (7, 8, '11-10-2021', 3, 'Post text from user 8');
-insert into Posts values (8, 6, '11-11-2021', 1, 'Post text from user 6');
-insert into Posts values (9, 2, '11-11-2021', 2, 'Post text from user 2');
-insert into Posts values (10, 4, '11-13-2021', 8, 'Post text from user 4');
+insert into Posts values (1, 6, '11-1-2021', 3, 'nibh venenatis cras sed felis eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat');
+insert into Posts values (2, 10, '11-3-2021', 1, 'dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas integer');
+insert into Posts values (3, 9,	'11-4-2021', 5,	'risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend mi in nulla posuere');
+insert into Posts values (4, 7, '11-6-2021', 9, 'quis risus sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum non');
+insert into Posts values (5, 6, '11-7-2021', 10, 'sagittis id consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper quis lectus nulla at');
+insert into Posts values (6, 3, '11-8-2021', 2, 'vel facilisis volutpat est velit egestas dui id ornare arcu odio ut sem nulla pharetra diam sit amet nisl suscipit');
+insert into Posts values (7, 8, '11-10-2021', 3, 'elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at consectetur lorem donec massa');
+insert into Posts values (8, 6, '11-11-2021', 1, 'eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui sapien eget mi');
+insert into Posts values (9, 2, '11-11-2021', 2, 'aliquet nibh praesent tristique magna sit amet purus gravida quis blandit turpis cursus in hac habitasse platea dictumst quisque sagittis');
+insert into Posts values (10, 4, '11-13-2021', 8, 'rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed faucibus turpis');
 
 insert into Liked values (1, 10, 6);
 insert into Liked values (2, 6, 4);
@@ -92,27 +106,27 @@ insert into Liked values (8, 7, 5);
 insert into Liked values (9, 1, 10);
 insert into Liked values (10, 7, 3);
 
-insert into Comments values (1, 7, 5, '11-1-2021', 'Comment from user 5');
-insert into Comments values (2, 2, 10, '11-2-2021', 'Comment from user 10');
-insert into Comments values (3, 9, 2, '11-3-2021', 'Comment from user 2');
-insert into Comments values (4, 9, 3, '11-6-2021', 'Comment from user 3');
-insert into Comments values (5, 8, 10, '11-7-2021', 'Comment from user 10');
-insert into Comments values (6, 10, 9, '11-8-2021', 'Comment from user 9');
-insert into Comments values (7, 5, 10, '11-9-2021', 'Comment from user 10');
-insert into Comments values (8, 1, 10, '11-10-2021', 'Comment from user 10');
-insert into Comments values (9, 5, 5, '11-14-2021', 'Comment from user 5');
-insert into Comments values (10, 7, 10, '11-15-2021', 'Comment from user 10');
+insert into Comments values (1, 7, 5, '11-1-2021', 'blandit turpis cursus in hac habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat mauris nunc congue nisi vitae');
+insert into Comments values (2, 2, 10, '11-2-2021', 'facilisis sed odio morbi quis commodo odio aenean sed adipiscing diam donec adipiscing tristique risus nec feugiat in fermentum posuere');
+insert into Comments values (3, 9, 2, '11-3-2021', 'felis eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce id velit ut tortor');
+insert into Comments values (4, 9, 3, '11-6-2021', 'eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus');
+insert into Comments values (5, 8, 10, '11-7-2021', 'nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices');
+insert into Comments values (6, 10, 9, '11-8-2021', 'vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at');
+insert into Comments values (7, 5, 10, '11-9-2021', 'rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel');
+insert into Comments values (8, 1, 10, '11-10-2021', 'duis convallis convallis tellus id interdum velit laoreet id donec ultrices tincidunt arcu non sodales neque sodales ut etiam sit');
+insert into Comments values (9, 5, 5, '11-14-2021', 'luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit');
+insert into Comments values (10, 7, 10, '11-15-2021', 'sit amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris a diam maecenas sed enim ut sem viverra');
 
-insert into CommentReplies values(1, 1, 'Comment reply from user 1');
-insert into CommentReplies values(2, 9, 'comment reply from user 9');
-insert into CommentReplies values(3, 7, 'comment reply from user 7');
-insert into CommentReplies values(4, 10, 'comment reply from user 10');
-insert into CommentReplies values(5, 6, 'comment reply from user 6');
-insert into CommentReplies values(6, 4, 'comment reply from user 4');
-insert into CommentReplies values(7, 2, 'comment reply from user 2');
-insert into CommentReplies values(8, 10, 'comment reply from user 10');
-insert into CommentReplies values(9, 4, 'comment reply from user 4');
-insert into CommentReplies values(10, 1, 'comment reply from user 1');
+insert into CommentReplies values(1, 1, 'ut ornare lectus sit amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor');
+insert into CommentReplies values(2, 9, 'sed id semper risus in hendrerit gravida rutrum quisque non tellus orci ac auctor augue mauris augue neque gravida in');
+insert into CommentReplies values(3, 7, 'at urna condimentum mattis pellentesque id nibh tortor id aliquet lectus proin nibh nisl condimentum id venenatis a condimentum vitae');
+insert into CommentReplies values(4, 10, 'id aliquet lectus proin nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus');
+insert into CommentReplies values(5, 6, 'tristique et egestas quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci nulla pellentesque dignissim enim sit amet venenatis');
+insert into CommentReplies values(6, 4, 'vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu');
+insert into CommentReplies values(7, 2, 'et odio pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis');
+insert into CommentReplies values(8, 10, 'consequat mauris nunc congue nisi vitae suscipit tellus mauris a diam maecenas sed enim ut sem viverra aliquet eget sit');
+insert into CommentReplies values(9, 4, 'diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo');
+insert into CommentReplies values(10, 1, 'tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus quam id leo');
 
 insert into Topics values(1, 'Science');
 insert into Topics values(2, 'Art');
@@ -146,34 +160,48 @@ insert into Followers values(7, 1, 2);
 insert into Followers values(8, 10, 3); 
 insert into Followers values(9, 7, 3); 
 insert into Followers values(10, 6, 5); 
--- select
-SELECT Posts.post_id
-FROM Posts, Users
-WHERE Posts.post_author_id=Users.user_id and Users.user_id=6;
---get all the posts by a certain user(user 6 for example)
 
+-- ================
+-- QUERIES
+-- ================
+
+-- Get all the posts by a certain user (user 6 for example)
+SELECT post_id 
+FROM Posts, Users
+WHERE Posts.post_author_id=Users.user_id 
+    and Users.user_id=6;
+
+-- Get all the instances of a specific topic from a user (user 6 for example)
 SELECT Topics.topic_id
 FROM Topics, Users, PostsWithTopics, Posts 
-WHERE Topics.topic_id=PostsWithTopics.topic_post_topic_id and Posts.post_id=Users.user_id and PostsWithTopics.topic_post_post_id=Posts.post_id and Users.user_id=6 and Topics.topic_name='Sports'; 
---Get all the instances of a specific topic from a user(user 6 for example)
+WHERE Topics.topic_id=PostsWithTopics.topic_post_topic_id 
+    and Posts.post_id=Users.user_id 
+    and PostsWithTopics.topic_post_post_id=Posts.post_id 
+    and Users.user_id=6 
+    and Topics.topic_name='Sports'; 
 
+--Get all the users matching a certain name
 SELECT user_name
 FROM Users
 WHERE user_name='Tomomi Stanković';
---Get all the users matching a certain name
 
+-- Get all the comments for a specific post
 SELECT Comments.comment_text
 FROM Posts, Comments
-WHERE Posts.post_id=Comments.comment_post_id and Posts.post_id=1;
--- Get all the comments for a specific post
+WHERE Posts.post_id=Comments.comment_post_id 
+    and Posts.post_id=1;
 
---****I didn't use number 4. I did 1, 2, 3,and 5****
+-- Get all the posts matching a certain search phrase
+    -- Example search phrase: 'from user 10'
 
 --Get all the posts made by users on a certain date
+
+
 --Get all the posts matching a certain search phrase(possibly this one instead)
 
--- update
+
+-- Update
 
 
--- delete
+-- Delete
 
