43c0eaa1d31018e2af5c2df5b3f3e7c4b737b093
robokae <alexanderhom19@gmail.com>
2021-11-29 22:57:14 -0800

    navbar markup and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPeopleCarry } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 
 function Navbar() {
     return (
         <div className="navbar">
             <nav>
-                <NavLink to="/home">Encapsulate</NavLink>
+                <div className="navbar-left">
+                    <NavLink className="logo" to="/home">
+                        <FontAwesomeIcon className="logo-icon" icon={faPeopleCarry} />
+                        <p className="logo-text">Encapsulate</p>
+                    </NavLink>
+                </div>
+                <div className="navbar-right">
+                    <input className="search-bar" placeholder="Search..." autoComplete="off" />
+                    <button className="sign-in-button">Sign in</button> 
+                </div>
             </nav> 
         </div>
     );
